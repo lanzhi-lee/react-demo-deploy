4ec2db583922687850690ccc47c586438df41f41
baibai-lee <baibai-lee@users.noreply.github.com>
2020-05-21 08:50:03 +0000

    Automated deployment: Thu May 21 08:50:03 UTC 2020 72159a547bda80803e42317dc7008fdc545b5da1

---
+++ precache-manifest.d5bf3b159a424d51e398f048e0ac64c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9d11975b12508daf28e5b10f75a996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bff9345dc2b3ee6ca859",
+    "url": "/static/css/main.578c204f.chunk.css"
+  },
+  {
+    "revision": "cfd63a6a2960db4380e0",
+    "url": "/static/js/2.55aab0ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.55aab0ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bff9345dc2b3ee6ca859",
+    "url": "/static/js/main.a479eedc.chunk.js"
+  },
+  {
+    "revision": "65aa19016c29a0ca5e37",
+    "url": "/static/js/runtime-main.4cae0e6d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
