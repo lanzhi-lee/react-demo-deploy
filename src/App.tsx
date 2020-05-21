31251a066bea01af620b0d2d2abcdbf1ce605e49
Li Zhan <lizhan1@xiaomi.com>
2020-05-21 15:48:59 +0800

    chore: 更新项目描述

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>This is a demo to demonstrate</p>
+        <p>How to deploy a project using GitHub Pages and GitHub Actions</p>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
